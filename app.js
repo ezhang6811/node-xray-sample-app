dd8cb6f951e73816fa04bd961956a84a03fd709b
Eric Zhang <eric.zhang6811@gmail.com>
2024-12-10 14:05:31 -0800

    minor fixes and comments

--- app.js
+++ app.js
@@ -25,23 +25,21 @@ app.get('/generate-automatic-traces', async (req, res) => {
 });
 
 app.get('/generate-manual-traces', async (req, res) => {
+  // create manual subsegment
   const segment = AWSXRay.getSegment();
   const subsegment = segment.addNewSubsegment('ManualSegment');
 
   try {
-    const s3 = new S3Client({ region: 'us-west-2' });
-    const data = await s3.send(new ListBucketsCommand());
+    const s3Client = new S3Client({ region: 'us-west-2' });
+    const data = await s3Client.send(new ListBucketsCommand());
 
     const buckets = data.Buckets.map(bucket => ({
       name: bucket.Name,
       creation_date: bucket.CreationDate.toISOString()
     }));
-
-    segment.close();
     res.json(buckets);
   } catch (err) {
-    segment.addError(err);
-    segment.close();
+    subsegment.addError(err);
     res.status(500).send(`Unable to list buckets: ${err.message}`);
   }
 
@@ -52,7 +50,7 @@ app.get('/generate-manual-traces', async (req, res) => {
 app.use(XRayExpress.closeSegment());
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
