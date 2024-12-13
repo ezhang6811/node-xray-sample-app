57db709bb8d3e6dbcfa8955daff291c6f1f343a6
Eric Zhang <eric.zhang6811@gmail.com>
2024-12-13 10:43:41 -0800

    create nested subsegment for manul trace

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const XRayExpress = require('aws-xray-sdk-express');
 const express = require('express');
 const { S3Client, ListBucketsCommand } = require('@aws-sdk/client-s3');
 
-// Wrap the Express app with X-Ray middleware
 const app = express();
+// start segment for subsequent requests
 app.use(XRayExpress.openSegment('MyApp'));
 
+// create an S3 client that automatically captures API calls
+const s3Client = AWSXRay.captureAWSv3Client(new S3Client({ region: 'us-west-2' }));
+
+// automatic subsegment management
 app.get('/generate-automatic-traces', async (req, res) => {
   try {
-    // Capture outgoing requests from S3 client
-    const s3Client = AWSXRay.captureAWSv3Client(new S3Client({ region: 'us-west-2' }));
     const data = await s3Client.send(new ListBucketsCommand());
 
     const buckets = data.Buckets.map(bucket => ({
@@ -24,26 +26,26 @@ app.get('/generate-automatic-traces', async (req, res) => {
   }
 });
 
+// manual subsegment management
 app.get('/generate-manual-traces', async (req, res) => {
   // create manual subsegment
-  const segment = AWSXRay.getSegment();
-  const subsegment = segment.addNewSubsegment('ManualSegment');
+  AWSXRay.captureAsyncFunc('CustomSubsegment', async (subsegment) => {
+    try {
+      const data = await s3Client.send(new ListBucketsCommand());
 
-  try {
-    const s3Client = new S3Client({ region: 'us-west-2' });
-    const data = await s3Client.send(new ListBucketsCommand());
+      const buckets = data.Buckets.map(bucket => ({
+        name: bucket.Name,
+        creation_date: bucket.CreationDate.toISOString()
+      }));
+      res.json(buckets);
+    } catch (err) {
+      subsegment.addError(err);
+      res.status(500).send(`Unable to list buckets: ${err.message}`);
+    }
 
-    const buckets = data.Buckets.map(bucket => ({
-      name: bucket.Name,
-      creation_date: bucket.CreationDate.toISOString()
-    }));
-    res.json(buckets);
-  } catch (err) {
-    subsegment.addError(err);
-    res.status(500).send(`Unable to list buckets: ${err.message}`);
-  }
+    subsegment.close();
+  });
 
-  subsegment.close();
 });
 
 // End the X-Ray segment for the request
